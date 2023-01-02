4d9feca63177796a9525ac04cf990ff5d5a21cc5
Srikanth K <padmanav.t@techouts.co.in>
2023-01-02 15:42:39 +0530

    commited

--- src/mart/supermarket/Avalability.java
+++ src/mart/supermarket/Avalability.java
@@ -1,204 +1,142 @@
 package mart.supermarket;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-class Avalability{
+class Avalability {
 	Scanner sc = new Scanner(System.in);
-	String str=null;
-	ArrayList<Products> Selected=new ArrayList<Products>();
-	public ArrayList<Products> avalableproducts(ArrayList<Products> Cloth,ArrayList<Products> Food,ArrayList<Products> Homeappliances,ArrayList<Products> Electronicgadgets)
-	{
-		do
-		{
+	String str = null;
+	ArrayList<Products> Selected = new ArrayList<Products>();
+
+	public ArrayList<Products> avalableproducts(ArrayList<Products> Cloth, ArrayList<Products> Food,
+			ArrayList<Products> Homeappliances, ArrayList<Products> Electronicgadgets) {
+		do {
 			try {
-			
-			System.out.println("Which type of products do you wants to buy :- ");
-			System.out.println("\t (1) cloths\n\t (2) Food\n\t (3)Homeapliances\n\t (4) Electronics");
-			System.out.println("Enter your choice :- ");
-			int choice1=sc.nextInt();
-			switch(choice1)
-			{
-			case 1:
-				int flag=0;
-				System.out.println("Avalable Products in cloths are :-\n");
-				System.out.println("Product_id"+"\tAvalability"+"\t\t\t Price"+"\t \tDescription");
-				System.out.println("_____________________________________________________________________________________________________________");
-				for(Products p1:Cloth)
-				{
-					System.out.println(p1.getProductid()+" \t\t "+p1.getNumberofproduct()+"\t\t\t\t "+p1.getPrice()+"\t \t"+p1.getProductdescription());
-					System.out.println("------------------------------------------------------------------------------------------------------------");
-				}
-				System.out.println("Enter The Product id which you wants to buy :- ");
-				int selectedproduct=sc.nextInt();
-				System.out.println("Enter the Quantity:-");
-				int quantity=sc.nextInt();
-				for(Products p1:Cloth)
-				{
-					if(p1.getProductid()==selectedproduct && (p1.getNumberofproduct()-quantity)>0)
-					{
-						if(Selected.contains(p1))
-						{
-							flag=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity);
+				int choice1 = 0;
+				System.out.println("Which type of products do you wants to buy :- ");
+				System.out.println("\t (1) cloths\n\t (2) Food\n\t (3)Homeapliances\n\t (4) Electronics");
+				do {
+					try {
+						System.out.println("Enter your choice :- ");
+						choice1 = sc.nextInt();
+						if (choice1 > 0 && choice1 < 5) {
+							break;
+						} else {
+							System.err.println("\n!!!!----Please enter a valid choice----!!!!\n");
 						}
-						else
-						{
-							flag=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity);
-							Selected.add(p1);
-						}
-						System.out.println("\n----Product is sucessfully added to cart----\n");
-						break;
+					} catch (Exception e) {
+						System.err.println("\n!!!!----Please enter a valid choice----!!!!\n");
+						sc.nextLine();
 					}
-					else {
-						flag=0;
-					}
+				} while (true);
+
+				switch (choice1) {
+				case 1:
+					buy(Cloth);
+					break;
+
+				case 2:
+					buy(Food);
+					break;
+
+				case 3:
+					buy(Homeappliances);
+					break;
+
+				case 4:
+					buy(Electronicgadgets);
+					break;
+
+				default:
+					System.err.println("Wrong choice");
 				}
-				if(flag==0)
-				{
-					System.err.println("sorry you have entered wrong input or the sellected product is empty ");
-				}
-				
-				break;
-			case 2:
-				int flag1=0;
-				System.out.println("Avalable Products in Food are :-\n");
-				System.out.println("Product_id"+"\tAvalability"+"\t\t\t Price"+"\t \tDescription");
-				System.out.println("_____________________________________________________________________________________________________________");
-				for(Products p1:Food)
-				{
-					System.out.println(p1.getProductid()+" \t\t "+p1.getNumberofproduct()+"\t\t\t\t "+p1.getPrice()+"\t \t"+p1.getProductdescription());
-					System.out.println("------------------------------------------------------------------------------------------------------------");
-				}
-				System.out.println("Enter The Product id which you wants to buy :- ");
-				int selectedproduct1=sc.nextInt();
-				System.out.println("Enter the Quantity:-");
-				int quantity1=sc.nextInt();
-				for(Products p1:Food)
-				{
-					if(p1.getProductid()==selectedproduct1 && (p1.getNumberofproduct()-quantity1)>0)
-					{
-						if(Selected.contains(p1))
-						{
-							flag1=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity1);
-						}
-						else
-						{
-							flag1=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity1);
-							Selected.add(p1);
-						}
-						System.out.println("\n----Product is sucessfully added to cart----\n");
-						break;
-					}
-					else {
-						flag1=0;
-					}
-				}
-				if(flag1==0)
-				{
-					System.err.println("sorry you have entered wrong input or the sellected product is empty ");
-				}
-				break;
-			case 3:
-				int flag3=0;
-				System.out.println("Avalable Products in Homeappliances are :-\n");
-				System.out.println("Product_id"+"\tAvalability"+"\t\t\t Price"+"\t \tDescription");
-				System.out.println("_____________________________________________________________________________________________________________");
-				for(Products p1:Homeappliances)
-				{
-					System.out.println(p1.getProductid()+" \t\t "+p1.getNumberofproduct()+"\t\t\t\t "+p1.getPrice()+"\t \t"+p1.getProductdescription());
-					System.out.println("------------------------------------------------------------------------------------------------------------");
-				}
-				System.out.println("Enter The Product id which you wants to buy :- ");
-				int selectedproduct3=sc.nextInt();
-				System.out.println("Enter the Quantity:-");
-				int quantity11=sc.nextInt();
-				for(Products p1:Homeappliances)
-				{
-					if(p1.getProductid()==selectedproduct3 && (p1.getNumberofproduct()-quantity11)>0)
-					{
-						if(Selected.contains(p1))
-						{
-							flag3=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity11);
-						}
-						else
-						{
-							flag3=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity11);
-							Selected.add(p1);
-						}
-						System.out.println("\n----Product is sucessfully added to cart----\n");
-						
-						break;
-					}
-					else {
-						flag3=0;
-					}
-				}
-				if(flag3==0)
-				{
-					System.err.println("sorry you have entered wrong input or the sellected product is empty ");
-				}
-				break;
-			case 4:
-				int flag4=0;
-				System.out.println("Avalable Products in Electronicgadgets are :-\n");
-				System.out.println("Product_id"+"\tAvalability"+"\t\t\t Price"+"\t \tDescription");
-				System.out.println("_____________________________________________________________________________________________________________");
-				for(Products p1:Electronicgadgets)
-				{
-					System.out.println(p1.getProductid()+" \t\t "+p1.getNumberofproduct()+"\t\t\t\t "+p1.getPrice()+"\t \t"+p1.getProductdescription());
-					System.out.println("------------------------------------------------------------------------------------------------------------");
-				}
-				System.out.println("Enter The Product id which you wants to buy :- ");
-				int selectedproduct4=sc.nextInt();
-				System.out.println("Enter the Quantity:-");
-				int quantity111=sc.nextInt();
-				
-				for(Products p1:Electronicgadgets)
-				{
-					if(p1.getProductid()==selectedproduct4 && (p1.getNumberofproduct()-quantity111)>0)
-					{
-						if(Selected.contains(p1))
-						{
-							flag4=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity111);
-						}
-						else
-						{
-							flag4=1;
-							p1.setNumberofproduct(p1.getNumberofproduct()-quantity111);
-							Selected.add(p1);
-						}
-						System.out.println("\n----Product is sucessfully added to cart----\n");
-						break;
-					}
-					else {
-						flag4=0;
-					}
-				}
-				if(flag4==0)
-				{
-					System.err.println("sorry you have entered wrong input or the sellected product is empty ");
-				}
-				break;
-			default:
-				System.err.println("Wrong choice");
-			}
-			}catch(Exception e)
-			{
+			} catch (Exception e) {
 				System.err.println("!!!!!-------Illegal argument, Enter a valid Argument ----------!!!!");
 			}
-		System.out.println("Do you Wants to continue (y/n):- ");
-		sc.nextLine();
-		str=sc.nextLine();
-			
-		}while(str.equals("y"));
-		
+			System.out.println("Do you Wants to continue (y/n):- ");
+			sc.nextLine();
+			str = sc.nextLine();
+
+		} while (str.equals("y"));
+
 		return Selected;
+	}
+
+	private void buy(ArrayList<Products> list) {
+		int flag = 0;
+		System.out.println("Avalable Products are :-\n");
+		System.out.println("Product_id" + "\tAvalability" + "\t\t\t Price" + "\t \tDescription");
+		System.out.println(
+				"_____________________________________________________________________________________________________________");
+		for (Products p1 : list) {
+			System.out.println(p1.getProductid() + " \t\t " + p1.getNumberofproduct() + "\t\t\t\t " + p1.getPrice()
+					+ "\t \t" + p1.getProductdescription());
+			System.out.println(
+					"------------------------------------------------------------------------------------------------------------");
+		}
+		do {
+			try {
+
+				System.out.println("Enter The Product id which you wants to buy :- ");
+				int selectedproduct = sc.nextInt();
+				int quantity=0;
+				for (Products p1 : list) {
+					if (p1.getProductid() == selectedproduct && p1.getNumberofproduct()>0) {
+						do {
+							try {
+								System.out.println("Enter your a number beetween (1 - "+p1.getNumberofproduct()+" ) :- ");
+								quantity = sc.nextInt();
+								if (quantity > 0 && quantity <= 5 && (p1.getNumberofproduct() - quantity >= 0)) {
+									break;
+								} else {
+									System.err.println("Please enter a valid choice");
+								}
+							} catch (Exception e) {
+								System.err.println("Please enter a valid choice");
+								sc.nextLine();
+							}
+						}while(true);
+
+						if (Selected.contains(p1)) {
+							flag = 1;
+							p1.setNumberofproduct(p1.getNumberofproduct() - quantity);
+							print(quantity);
+							System.out.println("\n\t\t\t----Product is sucessfully added to cart----\n");
+						} else if (!Selected.contains(p1)) {
+							flag = 1;
+							p1.setNumberofproduct(p1.getNumberofproduct() - quantity);
+							Selected.add(p1);
+							print(quantity);
+							System.out.println("\n\t\t\t----Product is sucessfully added to cart----\n");
+						} else {
+							flag = 0;
+						}
+					}
+				
+			} 
+				if (flag == 0) {
+					System.err.println("\t!!----Enter valid product id----!!");
+				}
+			}catch (Exception e) {
+				System.err.println("\t!!----Enter a valid productid----!!");
+				sc.nextLine();
+			}
+		} while (flag == 0);
+	}
+	
+	public void print(int quantity)
+	{
+		System.out.println("\n\n\t\t\t\t\t\tYour Cart");
+		System.out.println(
+				"------------------------------------------------------------------------------------------------------------");
+		for (Products p1 : Selected) {
+			
+			System.out.println(p1.getProductid() + " \t\t " + (5-p1.getNumberofproduct() )+ "\t\t " + p1.getPrice()
+					+ "\t \t"+p1.getPrice()*quantity+"\t\t" + p1.getProductdescription());
+			System.out.println(
+					"------------------------------------------------------------------------------------------------------------");
+		}
 	}
 }
--- src/mart/supermarket/BillGeneration.java
+++ src/mart/supermarket/BillGeneration.java
@@ -13,35 +13,38 @@ public class BillGeneration {
 		try {
 			System.out.println("Enter Your name :- ");
 			name = sc.nextLine();
-			while(!name.matches("^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$")) {
+			while(!name.matches("[A-Za-z]+")) {
 				System.out.println("Please Enter a correct name :- ");
 				name = sc.nextLine();
 			}
 			System.out.println("Enter Your Mobile number :- ");
 			mob = sc.nextLine();
 			
-			while(10 != mob.length()) {
+			while(10 != mob.length() || !(mob.matches("[0-9]{10}"))) {
 				System.out.println("Please Enter a correct Number :- ");
 				mob = sc.next();
 			}
 
 			double totalprice = 0;
 			int i = 1;
-			System.out.println("\t\tBIL GENERATE FOR ");
-			System.out.println("\t-------------------------------------------------");
-			System.out.println("\t\tName  :   " + name);
-			System.out.println("\t\tMobno :" + mob);
-			System.out.println("\t-------------------------------------------------\n");
+			int totalproducts=0;
+			System.out.println("\t\t\tBIL GENERATE FOR ");
+			System.out.println("\t\t-------------------------------------------------");
+			System.out.println("\t\t\tName  :" + name);
+			System.out.println("\t\t\tMobno :" + mob);
+			System.out.println("\t\t-------------------------------------------------\n");
 			for (Products p : selected) {
 				int numberofproduct=5-p.getNumberofproduct();
 				System.out.println("------------------------------------------------------------------------");
 				System.out.println(
-						i + "\t" + p.getProductid() + "\t\t" + p.getProductdescription() + "\t" + p.getPrice()+"\t\t"+numberofproduct+"\t"+(numberofproduct*p.getPrice()));
+						i + "\t" + p.getProductid() + "\t\t" + p.getProductdescription() + "\t" + p.getPrice()+"\t\t"+numberofproduct+"\t\t"+(numberofproduct*p.getPrice()));
 				totalprice += (numberofproduct*p.getPrice());
 				i++;
+				totalproducts +=(5-p.getNumberofproduct());
 			}
-			System.out.println("------------------------------------------------_________________________");
-			System.out.println("                                              Total:-    " + totalprice);
+			
+			System.out.println("--------------------------------------------_______________--------------___________________");
+			System.out.println("    			                  Total products: "+totalproducts+"        Total:-" + totalprice);
 			
 			
 			
@@ -50,10 +53,10 @@ public class BillGeneration {
 				File bill = new File("Bill.txt");
 				bill.createNewFile();
 				FileWriter fw = new FileWriter("Bill.txt");
-				fw.write("\t\tBIL GENERATE FOR ");
-				fw.append("\n\t-------------------------------------------------\n");
-				fw.append("\t\tName  :   " + name + "\n");
-				fw.append("\t\tMobno :" + mob + "\n");
+				fw.write("\t\t\tBIL GENERATE FOR ");
+				fw.append("\n\t\t-------------------------------------------------\n");
+				fw.append("\t\t\tName  :" + name);
+				fw.append("\n\t\t\tMobno :" + mob+"\n");
 				i = 1;
 				for (Products p : selected) {
 					int numberofproduct=5-p.getNumberofproduct();
@@ -62,8 +65,8 @@ public class BillGeneration {
 					totalprice += (numberofproduct*p.getPrice());
 					i++;
 				}
-				fw.append("\n------------------------------------------------_________________________\n");
-				fw.append("                                              Total:-    " + totalprice);
+				fw.append("\n--------------------------------------------_______________--------------___________________\n");
+				fw.append("    			                  Total products: "+totalproducts+"        Total:-" + totalprice);
 				fw.close();
 			} catch (Exception e) {
 				System.err.println("File generetion failure");
